20f321a439bcff9871fb002826f970a59257ba97
chn <chn@outsi.de>
2023-04-07 11:47:55 +0300

    feat: lint

--- src/main/java/com/dsa/app/algorithms/array/RemoveDuplicatesSortedArray.java
+++ src/main/java/com/dsa/app/algorithms/array/RemoveDuplicatesSortedArray.java
@@ -5,40 +5,40 @@ import java.util.Arrays;
 public class RemoveDuplicatesSortedArray {
     public int solution(int[] nums) {
         int res = nums.length;
-
-        for(int i = 1; i < res; i++) {
-           if(nums[i] == nums[i - 1]) {
-               int tmp = nums[i];
-
-               for(int j = i + 1; j < nums.length; j++) {
-                   nums[j -1] = nums[j];
-               }
-
-               nums[nums.length - 1] = tmp;
-
-               i--;
-               res--;
-           }
+        
+        for (int i = 1; i < res; i++) {
+            if (nums[i] == nums[i - 1]) {
+                int tmp = nums[i];
+                
+                for (int j = i + 1; j < nums.length; j++) {
+                    nums[j - 1] = nums[j];
+                }
+                
+                nums[nums.length - 1] = tmp;
+                
+                i--;
+                res--;
+            }
         }
-
+        
         return res;
     }
-
+    
     public int soluction2(int[] nums) {
         if (nums == null) {
             return 0;
         }
-
+        
         int writePointer = 1;
-
+        
         for (int readPointer = 1; readPointer < nums.length; readPointer++) {
             if (nums[readPointer] != nums[readPointer - 1]) {
                 nums[writePointer] = nums[readPointer];
                 writePointer++;
             }
         }
-
-        // This turns out to be the correct length value.
+        
+        
         return writePointer;
     }
 }
